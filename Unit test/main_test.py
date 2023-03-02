cd0f5ae8eed1048327b52f450ee2253f6a9d478e
Leolas64 <114671462+Leolas64@users.noreply.github.com>
2023-03-02 11:35:32 +0100

    Modif Main_test

--- Unit test/main_test.py
+++ Unit test/main_test.py
@@ -1,40 +1,56 @@
 import unittest
-import json
-import pymongo
+from pymongo import MongoClient
 from fastapi.testclient import TestClient
 from main import app
-
-client = TestClient(app)
-
-class TestEmployeeFunctions(unittest.TestCase):
-    
+import json
+        
+class TestCrudRequest(unittest.TestCase):
+   
     def setUp(self):
-        self.mongo_client = pymongo.MongoClient('localhost', 27017)
-        self.db = self.mongo_client['hrms']
-        self.collection = self.db['employee']
+        self.client = MongoClient("localhost", 27017)
+        self.db = self.client["hrms"]
+        self.collection = self.db["employee"]
+        self.test_client = TestClient(app)
+        
+        
+    def test_post(self):
+        retour = self.test_client.post("/create_employee", params={"name": "John Doe", "age": "26", "teams": ["Marketing"]} )
+        self.assertEqual(retour.status_code, 200)
+        self.assertEqual(retour.json(), ["Test Ajout"]) 
+        ajout = self.collection.find_one({"name": "John Doe"})
+        self.assertIsNotNone(ajout)
+        self.assertEqual(ajout["name"], "John Doe")
+        self.assertEqual(ajout["age"], "26")
+        self.assertEqual(ajout["Teams"], ["Marketing"])
+        
+    def test_get(self):
+        self.collection.insert_one({"name": "John Doe", "age": "26", "teams": ["Marketing"]})
+        retour = self.test_client.get("/get_all_employees")
+        self.assertEqual(retour.status_code, 200)
+        test_utilisateur = json.loads(retour.content)
+        self.assertEqual(test_utilisateur[0]["name"], "John Doe")
 
-    def test_create_employee(self):
-        data = {"name": "John Doe", "age": 25, "teams": ["Marketing", "Sales"]}
-        response = client.post("/create_employee", json=data)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(self.collection.count_documents(data), 1)
-    
-    def test_get_all_employees(self):
-        data = {"name": "John Doe", "age": 25, "teams": ["Marketing", "Sales"]}
-        self.collection.insert_one(data)
-        response = client.get("/get_all_employees")
-        self.assertEqual(response.status_code, 200)
-    
-    def test_delete_employee(self):
-        data = {"name": "John Doe", "age": 25, "teams": ["Marketing", "Sales"]}
-        self.collection.insert_one(data)
-        response = client.delete(f"/delete_employee?name={data['name']}")
-        self.assertEqual(response.status_code, 200)
-    
-    def test_update_employee(self):
-        old_data = {"name": "John Doe", "age": 25, "teams": ["Marketing", "Sales"]}
-        new_data = {"name": "John Smith", "age": 30, "teams": ["Sales", "Development"]}
-        self.collection.insert_one(old_data)
-        response = client.put(f"/update_employee?oldname={old_data['name']}", json=new_data)
-        self.assertEqual(response.status_code, 200)
 
+    def test_put(self):
+        retour = self.test_client.put("/update_employee", params = {"oldname": "John Doe", "name": "Pierre Dupont", "age": "50", "teams": ["Technologies"]})
+        self.assertEqual(retour.status_code, 200)
+        self.assertEqual(retour.json(), ["Modification effectuée"]) 
+        ajout = self.collection.find_one({"name": "Pierre Dupont"})
+        self.assertIsNotNone(ajout)
+        self.assertEqual(ajout["name"], "Pierre Dupont")
+        self.assertEqual(ajout["age"], "50")
+        self.assertEqual(ajout["teams"], ["Technologies"])
+
+    def test_delete(self):
+        
+        retour = self.test_client.delete("/delete_employee", params = {"name": "Pierre Dupont", "age": "50", "teams":["Technologies"]})
+        self.assertEqual(retour.status_code, 200)
+        self.assertEqual(retour.json(), ["Suppression 1"])
+        retour = self.test_client.delete("/delete_employee", params = {"name": "John Doe", "age": "25", "teams": ["Marketing"]})
+        self.assertEqual(retour.status_code, 200)
+        self.assertEqual(retour.json(), ["Suppression 2"])
+        self.assertEqual(self.collection.count_documents({"name": "Pierre Dupont", "age": "50", "teams":["Technologies"]}), 0)
+        self.assertEqual(self.collection.count_documents({"name": "John Doe", "age": "25", "teams": ["Marketing"]}), 0)
+        
+if __name__ == "__main__":
+    unittest.main()
